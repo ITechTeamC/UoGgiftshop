1a4099141165729f9bcf642b913691b8fa438c77
Junhan Kim <2253716k@student.gla.ac.uk>
2017-02-23 19:45:35 +0000

    Design layout applied

--- giftshop/views.py
+++ giftshop/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     context_dict = {'boldmessage': "it should be change to 'categories': category_list, 'item': item_list"}
-    response = render(request, 'rango/index.html', context_dict)
-    return response
+    return render(request, 'giftshop/index.html', context=context_dict)
